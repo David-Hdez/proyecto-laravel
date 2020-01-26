81a9a5eb442019e9d19d8c1844559f1c55ebe27f
David Alfredo Pacheco Hernandez <david.alfredo.hdez@outlook.com>
2020-01-25 21:55:25 -0600

    Configuración campos del usuario

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/configuracion', 'UserController@config')->name('config');
